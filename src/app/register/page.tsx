afbb022bd377143577ddcd6b9cec20d2e4966495
TrungDuc991 <dotrungduc.bl@gmail.com>
2023-05-26 14:08:02 +0700

    add lib package

---
+++ src/app/register/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        Instantly deploy your Next.js site to a shareable URL with Vercel.
+      </p>
+    </main>
+  );
+}
+// my-app/
+// | - .gitignore
+// | - README.md
+// | - node_modules
+// | - package.json
+// | - .env.local
+// | - .eslintrc
+// | - .prettierrc
+// | - next.config.js
+// | - public/
+//     | - favicon.ico
+//     | - vercel.svg
+// | - src/
+//     | - components/
+//         | - Layout.tsx
+//     | - pages/
+//         | - index.tsx
+//     | - styles/
+//         | - globals.css
+//         | - Home.module.css
+// | - tsconfig.json
